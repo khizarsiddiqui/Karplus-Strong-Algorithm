7dc240d340b96e7273755bd4daf37ee3fb903349
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-03 03:41:52 +0500

    implementing the ring buffer with deque

--- file-8.py
+++ file-8.py
@@ -10,3 +10,18 @@ file = wave.open('sine220.wav', 'wb')
 file.setparams((1, 2, sRate, nSamples, 'NONE', 'uncompressed'))
 file.writeframes(data)
 file.close()
+
+# The Minor Pentatonic Scale
+# implementing the ring buffer with deque
+
+from collections import deque
+d = deque(range(10))
+print(d)
+deque([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+d.append(-1)
+print(d)
+deque([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1])
+d.popleft()
+0
+print (d)
+deque([1, 2, 3, 4, 5, 6, 7, 8, 9, -1])
