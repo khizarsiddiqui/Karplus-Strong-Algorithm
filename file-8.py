55b2ad89848d64fc321199b1946b05b33e3c7471
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-04 02:45:25 +0500

    a deque container to implement the Karplus-Strong algorithm for the ring buffer

--- file-8.py
+++ file-8.py
@@ -1,6 +1,8 @@
 import numpy as np
 import wave, math
 
+# creating wav file
+
 sRate = 44100
 nSamples = sRate * 5
 x = np.arange(nSamples)/float(sRate)
@@ -15,6 +17,8 @@ file.close()
 # implementing the ring buffer with deque
 
 from collections import deque
+from collections import random
+
 d = deque(range(10))
 print(d)
 deque([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -25,3 +29,24 @@ d.popleft()
 0
 print (d)
 deque([1, 2, 3, 4, 5, 6, 7, 8, 9, -1])
+
+# now implemeting karplus algorithm
+# generate note of given frequency
+
+def generateNote(freq):
+    nSamples = 44100
+    sampleRate = 44100
+    N = int(sampleRate/freq)
+# initialize ring buffer
+    buf = deque([random.random() - 0.5 for i in range(N)])
+# initialize samples buffer
+    samples = np.array([0]*nSamples, 'float32')
+    for i in range(nSamples):
+        samples[i] = buf[0]
+        avg = 0.996*0.5*(buf[0] + buf[1])
+        buf.append(avg)
+        buf.popleft()
+# convert samples to 16-bit values and then to a string
+# the maximum value is 32767 for 16-bit
+    samples = np.array(samples*32767, 'int16')
+    return samples.tostring()
