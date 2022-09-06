c2f79909e70babd187384785bdee12036ba8e330
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-07 02:55:32 +0500

    Completed the code with some final add'ons

--- file-8.py
+++ file-8.py
@@ -4,7 +4,8 @@ import wave, argparse, pygame
 import math
 import numpy as np
 from collections import deque
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt 
+# from matplotlib import pyplot as plt
 
 # show plot of algorithm in action?
 gShowPlot = False
@@ -12,6 +13,13 @@ gShowPlot = False
 # notes of a Pentatonic Minor scale
 # piano C4-E(b)-F-G-B(b)-C5
 pmNotes = {'C4': 262, 'Eb': 311, 'F': 349, 'G':391, 'Bb':466}
+
+# Steps for simualtion
+# 1. Store the first value from the ring buffer in the samples buffer.
+# 2. Calculate the average of the first two elements in the ring buffer.
+# 3. Multiply this average value by an attenuation factor (in this case, 0.995).
+# 4. Append this value to the end of the ring buffer.
+# 5. Remove the first element of the ring buffer.
 
 # write WAVE file
 def writeWAVE(fileName, data):
@@ -35,7 +43,8 @@ def generateNote(freq):
     sampleRate = 44100
     N = int(sampleRate/freq)
 # initialize ring buffer
-    buf = deque([random.random() - 0.5 for i in range(N)]) # line comprehension
+    # line comprehension
+    buf = deque([random.random() - 0.5 for i in range(N)]) 
     # plot of flag set 
     if gShowPlot:
         axline, = plt.plot(buf)
@@ -43,18 +52,19 @@ def generateNote(freq):
     samples = np.array([0]*nSamples, 'float32')
     for i in range(nSamples):
         samples[i] = buf[0]
-        avg = 0.996*0.5*(buf[0] + buf[1])
+        # Multiply value by an attenuation factor (in this case, 0.995) , (0.5 being low pass filter).
+        avg = 0.995*0.5*(buf[0] + buf[1])
         buf.append(avg)
         buf.popleft()
-        # plot of flag set 
+        # plot of flag set using plt libraries
         if gShowPlot:
             if i % 1000 == 0:
                 axline.set_ydata(buf)
                 plt.draw()
 
-# convert samples to 16-bit values and then to a string
+# converting samples to 16-bit values and then to a string
 # the maximum value is 32767 for 16-bit
-    samples = np.array(samples*32767, 'int16')
+    samples = np.array(samples * 32767, 'int16')
     return samples.tostring()
 
 # play a wav file
@@ -112,7 +122,7 @@ def main():
             print('fileName already created. skipping...')
         
         # add note to player
-        nplayer.add(fileName + '.wav')
+        nplayer.add(name + '.wav')
         
         # play note if display flag set
         if args.display:
